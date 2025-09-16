0a905f8f0547aca98a50937acc314ef536042ae6
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2025-09-16 13:02:17 +0700

    init commit

---
+++ BE/workshop-be-nosql/src/main/java/com/daonq1408/workshopbenosql/WorkshopBeNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.daonq1408.workshopbenosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopBeNosqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopBeNosqlApplication.class, args);
+    }
+
+}
